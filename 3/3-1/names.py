90bbc6a4b4c6c093c46d1fa0bc16c7bf91829f97
liqiwa <menghe163@gmail.com>
2018-06-26 07:54:31 +0800

    增加我的通勤方式

--- 3/3-1/names.py
+++ 3/3-1/names.py
@@ -6,4 +6,8 @@ print(names[2]+ word)
 print(names[4]+ word)
 print(names[3]+word)
 
-
+commuter = ["bicycle","car","motorcycle","bus"]
+need = "I would like to  "
+print(need+"own a Honda "+commuter[2])
+print(need+"own a mobai "+commuter[0])
+print(need+"own bmw "+commuter[1])
