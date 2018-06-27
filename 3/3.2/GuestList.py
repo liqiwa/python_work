ea49a8e1349759dc3f55a6242ed8a938db2f7c0f
liqiwa <menghe163@gmail.com>
2018-06-28 01:04:18 +0800

    嘉宾名单

---
+++ 3/3.2/GuestList.py
@@ -0,0 +1,33 @@
+#guestlist
+guestlist=["jobs","tinyfool","huizhang","bill gates"]
+print("wo jiang yao yao qing "+guestlist[0]+" "+guestlist[1]+" "+guestlist[2]+" "+guestlist[3]+" "+"gong jin wan can")
+#changeList
+print(guestlist.pop(0)+" wu fa yi qi chi fan")
+print(guestlist)
+guestlist.insert(0,"jobess")
+print(guestlist[0]+" "+guestlist[1]+" "+guestlist[2]+" "+guestlist[3] +" huo de jin wan chi fan ji hui ")
+#lagetable
+print("I find a large table!!!")
+guestlist.insert(0,"masike")
+print(guestlist)
+guestlist.insert(3,"shimite")
+print(guestlist)
+guestlist.append("zak")
+print(guestlist)
+print(guestlist[0] +" "+guestlist[1]+" "+guestlist[2]+" "+guestlist[3]+ "get one time")
+#reduce
+print("I`m so sorry ,i only invita two Guest")
+print(guestlist.pop()+" i`m so sorry")
+print(guestlist)
+print(guestlist.pop()+"i`m so sorry")
+print(guestlist)
+print(guestlist.pop()+" I`m so sorry")
+print(guestlist.pop()+" I`m so sorry")
+print(guestlist.pop()+" I`m so sorry")
+print(guestlist)
+print(guestlist[0]+" you  a lucky ,you in guestlist")
+print(guestlist[1]+" you  a lucky ,you in guestlist")
+del guestlist[0]
+del guestlist[0]
+print(guestlist)
+
