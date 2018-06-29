185875da72e1843c9c3215acd34fae7e2a8be0b5
liqiwa <menghe163@gmail.com>
2018-06-30 01:01:25 +0800

    try

--- 3/3.3/world.py
+++ 3/3.3/world.py
@@ -20,3 +20,27 @@ print(worldplace)
 print("Here is use sort reverse word ")
 worldplace.sort(reverse = True)
 print(worldplace)
+####all function and method 
+worldcup = ["Germany","Brazil","Italy","Japan","Argentina","Holland","Portugal"]
+# yongjiuxing method
+worldcup.sort()
+print(worldcup)
+worldcup.sort(reverse = True)
+print(worldcup)
+worldcup.append("China")
+print(worldcup)
+worldcup.insert(0,"korea")
+print(worldcup)
+del worldcup[0]
+print(worldcup)
+print(worldcup.pop()+" out")
+print(worldcup)
+print(worldcup.pop(4)+" out ")
+print(worldcup)
+#int cast str 
+print("remain "+ str(len(worldcup))+ " Team")
+weedout = "Japan"
+print(worldcup.remove(weedout))
+print(weedout+"is  weedout")
+print(worldcup)
+print(sorted(worldcup))
