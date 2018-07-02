9688aae125923ee56f93258fbfa93cead7323977
liqiwa <menghe163@gmail.com>
2018-07-03 00:44:51 +0800

    lian xi + lizi

---
+++ 4/4.3/exercise.py
@@ -0,0 +1,14 @@
+#for value in range(1,21):
+ #print(value)
+
+numbers = [value for value in range(1,1000001)]
+ #print(numbers)
+#print("min is "+ str(min(numbers)))
+##print("max is " + str(max(numbers)))
+unevennumber = [value for value in range(1,20,2)]
+
+print(unevennumber)
+
+three = [value for value in range(3,30)]
+   rslt = mod(value,3)
+   print(three)
---
+++ 4/4.3/numbers.py
@@ -0,0 +1,5 @@
+for value in range(1,5):
+	print(value)
+
+nubmers = list(range(1,6))
+print(nubmers)
