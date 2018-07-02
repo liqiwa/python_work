691dc85415dba2de47785386b6930bbce041a2f7
liqiwa <menghe163@gmail.com>
2018-07-02 23:53:52 +0800

    练习

---
+++ 4/4.1/exercise.py
@@ -0,0 +1,12 @@
+pizzas = ["fruit","beef","shrimp"]
+for pizza in pizzas:
+ print("I love pizza ",pizza.title())
+ print("I like pepperoni pizza \n")
+
+print("I Really love pizza")
+
+Zoo = ["elephant","mouse","rabbit","tiger"]
+for animal in Zoo:
+ print(animal.title()) 
+ print(animal.title()+"A dog would make a great pet")
+print("Any of these animals would make great pet")
