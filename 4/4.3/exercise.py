111154b674d39df0067bf7b332e9d9d21d395ec5
liqiwa <menghe163@gmail.com>
2018-07-04 00:24:19 +0800

    add exercise

--- 4/4.3/exercise.py
+++ 4/4.3/exercise.py
@@ -9,6 +9,14 @@ unevennumber = [value for value in range(1,20,2)]
 
 print(unevennumber)
 
-three = [value for value in range(3,30)]
-   rslt = mod(value,3)
-   print(three)
+three = [value for value in range(3,30,3)]
+print(three)
+#
+
+cube = []
+for value in range(1,10):
+	cube3 = value**3
+	cube.append(cube3)
+print(cube)
+cube = [value**3 for value in range(1,10)]
+print(cube)
