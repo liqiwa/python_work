0551b001ac76812892ed98b9975b2ac897c8d5e7
liqiwa <menghe163@gmail.com>
2018-07-07 01:08:18 +0800

    4.5

---
+++ 4/4.5/dimensions.py
@@ -0,0 +1,22 @@
+dimensions = ('400','500')
+print(dimensions)
+for dimension in dimensions:
+	print(dimension)
+#dimensions[0] = 400
+dimensions = ('200','44')
+print(dimensions)
+
+dimensionss = dimensions[:]
+dimensionss = ('3322')
+print(dimensionss)
+print(dimensions)
+#exercise
+fivefoods = ('apple','water','noodle','orange','porridge')
+for foods in fivefoods:
+	print(foods)
+#fivefoods[3] = 'potato'
+fivefoods = ('apple','water','beer','beef','orange')
+for menu in fivefoods:
+	print(menu +"dd")
+print(sorted(fivefoods))
+print(fivefoods)
