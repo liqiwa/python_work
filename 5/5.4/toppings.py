f78718cfa637f6102db0929a97f9808bd37822bd
liqiwa <menghe163@gmail.com>
2018-07-12 00:28:50 +0800

    5 end

--- 5/5.4/toppings.py
+++ 5/5.4/toppings.py
@@ -20,8 +20,20 @@ else:
 current_users = ['xiaoh','xiaom','jack','jobs','bill','John']
 new_users = ['jack','bill','xiaoxiao','big','max','JOHN']
 for new_user in new_users:
+	current_users = [item.lower() for item in current_users]
 	if new_user.lower() in current_users:
-		
+
 		print(new_user+" is usage,please entry the other one")
 	else:
 		print(new_user+' is new_name')
+#####
+ordinal = [value for value in range(1,10)]
+for value in ordinal:
+    if value == 1:
+	    print(str(value)+'st')
+    elif value == 2:
+        print(str(value)+'nd')
+    elif value == 3:
+        print(str(value)+'rd')
+    else:
+        print(str(value)+'th')
