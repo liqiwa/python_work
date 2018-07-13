54858d33fce1810fb01b328283d840c5ab6abc61
liqiwa <menghe163@gmail.com>
2018-07-14 00:03:17 +0800

    alien

--- 6/alien.py
+++ 6/alien.py
@@ -1,5 +1,10 @@
 alien_o = {'color':'green','points' :10 }
 
 print(alien_o)
+alien_o [x_position]= 20
 
-alien_o = {}
+alien_o = [x_position] = 'green'
+alien_o = [y_position] = 'green'
+
+alien_o['color'] = 'yellow'
+print("The alien is now"+alien_o['color'] +".")
