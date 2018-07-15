860a7bee2f614d5078594aa15b0e90e9bc3d8267
liqiwa <menghe163@gmail.com>
2018-07-15 23:40:16 +0800

    add kong

--- 6/alien.py
+++ 6/alien.py
@@ -1,10 +1,12 @@
 alien_o = {'color':'green','points' :10 }
 
 print(alien_o)
-alien_o [x_position]= 20
 
-alien_o = [x_position] = 'green'
-alien_o = [y_position] = 'green'
+alien_o['x_position'] = 0
+alien_o['y_position'] = 25
 
 alien_o['color'] = 'yellow'
 print("The alien is now"+alien_o['color'] +".")
+print(alien_o)
+newPoint = alien_o['points']
+print("you are is get "+str(newPoint)+"points!!")
