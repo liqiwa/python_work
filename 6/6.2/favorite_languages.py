ae44dca9427855096186535e680dcc797fcbb115
liqiwa <menghe163@gmail.com>
2018-07-17 00:06:26 +0800

    alien

---
+++ 6/6.2/favorite_languages.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',}
+print("Sarah`s favorite_language is "+str(favorite_languages['sarah'].title())+'.')
+#6-1
+people = {'name':'mm','xing':'he','age':'30','city':'sjz'}
+print(people)
+
+words = {'liebiao':'liebiao,yuansu jihe','yuanzu':'bukebian xulie',''}
--- 6/alien.py
+++ 6/alien.py
@@ -9,4 +9,26 @@ alien_o['color'] = 'yellow'
 print("The alien is now"+alien_o['color'] +".")
 print(alien_o)
 newPoint = alien_o['points']
-print("you are is get "+str(newPoint)+"points!!")
+print("you are is get "+str(newPoint)+" points!!")
+##new alien
+alien_o = {}
+alien_o['color'] = 'green'
+alien_o['print'] = 5
+###alien 
+alien_o = {'x_position':0,'y_position':25,'speed':'medium'}
+print("Original x-psition:"+str(alien_o['x_position']))
+if alien_o['speed'] == 'medium':
+   x_increment = 2
+elif alien_o['speed'] =='slow':
+	x_increment = 1
+else:
+    x_increment = 3
+alien_o['x_position'] = alien_o['x_position'] + x_increment
+print("New x-psition is "+str(alien_o['x_position']))
+
+##del
+alien_o = {'color':'green','x_position':'24'}
+print(alien_o)
+del alien_o['x_position']
+print(alien_o)
+
