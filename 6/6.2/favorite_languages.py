56e1b62c805cf5e7878d9a7970f6010fe83b1e9c
liqiwa <menghe163@gmail.com>
2018-07-19 00:51:14 +0800

    zengjia

--- 6/6.2/favorite_languages.py
+++ 6/6.2/favorite_languages.py
@@ -10,4 +10,33 @@ print(people)
 
 words = {'liebiao':'liebiao,yuansu jihe','yuanzu':'bukebian xulie','zidian':'jianzhidui'}
 for k,v in words.items():
-	pass
+	print(k+"\n"+v)
+
+for name,language in favorite_languages.items():
+  print(name.title()+ "'s favorite language "+language.title())
+
+for language in set(favorite_languages.values()):
+  print(language)
+
+favorite_languages['zifuchuang'] = 'chulizifu'
+favorite_languages['for xunhuan']  = 'xunhuan henduo'
+print(favorite_languages)
+
+nations = {'nile':'egypt','changjiang':'china','Amazon River':'South America'}
+for river,nation in nations.items():
+  print("The "+river.title()+" runs through "+nation.title())
+for river in nations.keys():
+	print(river)
+for nation in nations.values():
+	print(nation+"\n")
+
+
+invite = ['jen','phil','for xunhuan']
+for name,language in favorite_languages.items():
+	if name in invite:
+		print(name+" Thank you!")
+	else:
+	  print(name+" invite join survey")
+print(favorite_languages)
+
+
