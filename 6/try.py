907d209191d8d75044696ce4b475bc92bd250b9f
liqiwa <menghe163@gmail.com>
2018-07-21 01:04:35 +0800

    add try

--- 6/try.py
+++ 6/try.py
@@ -15,3 +15,45 @@ for people,info in peoples.items():
     print(people)
     full_name = info['first_name']+info['last_name']
     print(full_name.title()+" is "+info['age']+" in "+info['city'])
+#6-8
+dog = {'kind':'tugou','master':'lei'}
+cat = {'kind':'jiaf','master':'qiaoz'}
+turtle = {'kind':'shuigui','master':'xiaoquan'}
+pets ={
+	 'dog':{'kind':'tugou','master':'lei'},
+	 'cat':{'kind':'jiaf','master':'qiaoz'},
+	 'turtle':{'kind':'shuigui','master':'xiaoquan'}
+	 }
+for pet_kind,pet_info in pets.items():
+	print(pet_kind)
+	print(pet_info['kind'] + " is  "+ pet_info['master'])
+
+#6-9
+favorite_places = {'jobs':['america','japan','cal'],
+'Musk':['mars','silicon valley'],
+'he':['china','taiwan','america']}
+
+for user,places in favorite_places.items():
+    print(user.title() +" is favorite ")
+    for place in places:
+    	print(place.title()+"\n\t")
+#6-10
+favorite_number = {
+	'jobs':['1','2','3'],
+	'Musk':['3','4','5'],
+	'he':['6','7','8']}
+for name,numbers in favorite_number.items():
+	print(name +"is  favorite ")
+	for number in numbers:
+		print(number)
+#6-11
+cities = {
+	'beijing':{'country':'china','population':'16-million','fact':'capital'},
+	'silicon valley':{'country':'America','population':'3.25-million','fact':'sitanfu'},
+	'tokyo':{'country':'japan','population':'1.27-million','fact':'dongjingdaxue'}
+}
+for city,infos in cities.items():
+	print(city)
+	print(infos['country']+"  "+infos['population']+"  "+infos['fact'])
+
+
