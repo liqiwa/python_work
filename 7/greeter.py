b3dde228e55ca57e2aa1359236bb47b6e96596d0
liqiwa <menghe163@gmail.com>
2018-07-25 23:57:02 +0800

    happy

---
+++ 7/greeter.py
@@ -0,0 +1,11 @@
+name = input("Plase enter your name:")
+print("Hello "+name)
+if name == 'He':
+	print("Hello" + name)
+
+age = input("How tall are you?")
+age = int(age)
+if age <=17:
+	print("you are is small")
+else:
+	print("you is big!!")
