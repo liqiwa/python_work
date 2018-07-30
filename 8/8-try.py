b0e0ca524c92923b42403c286952fbc80000f302
liqiwa <menghe163@gmail.com>
2018-07-31 00:00:58 +0800

    8 zhang

---
+++ 8/8-try.py
@@ -0,0 +1,13 @@
+###8-3
+def make_shirt(chima,word):
+	print(chima+word)
+make_shirt(word = " I ",chima = "l")
+
+###8-4
+
+def make_shirt(chima,word =" I love Python"):
+	print(chima+word)
+
+make_shirt("45")
+make_shirt("33")
+make_shirt("22"," I love Java")
---
+++ 8/greeter.py
@@ -0,0 +1,10 @@
+def  greeter(name):
+	"""我我我哦哦"""
+	print("Hello!!"+name)
+greeter(
+"wowoow"
+)
+
+def favorite_book(title):
+	print("One of my favorite books is "+title.title())
+favorite_book("iron man")
