116520b2f559572aec6255e888b562c16a2b9d20
liqiwa <menghe163@gmail.com>
2018-08-07 23:47:53 +0800

    add

---
+++ 8/pizza.py
@@ -0,0 +1,19 @@
+def make_pizza(*toppings):
+	print(toppings)
+
+make_pizza('xxxx''www')
+
+
+make_pizza("xigua","xihongshi","huanggua")
+
+##8-12
+def sanwich(*material):
+	print(material)
+
+sanwich('xihongshi')
+
+sanwich("tomato",'potato')
+
+sanwich('Bacon','lettuce','cuke')
+
+
