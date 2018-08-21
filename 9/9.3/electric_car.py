e50cdf927223495f7da0ce4335fc8d69f2df4286
liqiwa <menghe163@gmail.com>
2018-08-21 23:57:01 +0800

    modified

--- 9/9.3/electric_car.py
+++ 9/9.3/electric_car.py
@@ -17,13 +17,34 @@ class Car():
 			print("You cant`t roll back an odometer!")
 	def increment_odometer(self,miles):
 		self.odometer_reading += miles
+	
 
+class Battery():
+	"""模拟电瓶尝试"""
+	def __init__(self,battery_size = 70):
+		"""初始化电瓶属性"""
+		self.battery_size = battery_size
+	def describe_battery(self):
+		"""打印一条描述点瓶容量的消息"""
+		print("This car has a "+str(self.battery_size)+"-KWH battery")
+	def get_range(self):
+		"""打印1条信息，关于电池里程"""
+		if self.battery_size == 70:
+			range = 240
+		elif self.battery_size == 85:
+			range = 270
+		message = "This car can go approximately " +str(range)
+		message += " miles on a full charge"
+		print(message)
 class ElectricCar(Car):
 	"""电动汽车的独特之处"""
 	def  __init__(self,make,model,year):
-		"""初始化父类的属性"""
+		"""初始化父类的属性，再初始化电动汽车特有的属性"""
 		super().__init__(make,model,year)
+		self.battery = Battery()
 
 my_tesla = ElectricCar('tesla','model s','2016')
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 		
