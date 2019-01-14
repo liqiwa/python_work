f99910aa7e16b4d92df97b656cdd52802b7aa1de
liqiwa <menghe163@gmail.com>
2019-01-14 22:38:05 +0800

    add

--- 18/pizzeria/pizzerias/urls.py
+++ 18/pizzeria/pizzerias/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls import url
 
 urlpatterns = [
 	#主页
-	url(r'^$',views.index,name = 'index')
+	url(r'^$',views.index,name = 'index'),
+	#显示所有主题
+	url(r'^toppings/$',views.toppings,name = 'toppings'),
+	url(r'^toppings/(?P<topping_id>\d+)/$',views.topping,name = 'topping'),
 ]
--- 18/pizzeria/pizzerias/views.py
+++ 18/pizzeria/pizzerias/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from .models import Topping
 # Create your views here.
 def index(request):
 	"""披萨的店的主页"""
 	return render(request,'pizzerias/index.html')
+def toppings(request):
+	"""显示披萨的配料"""
+	topping = Topping.objects.order_by('date_added')
+	context = {'toppings':topping}
+	return render(request,'pizzerias/toppings.html',context)
+def topping(request,topping_id):
+	"""显示单个主题及其所有的条目"""
+	topping = Topping.objects.get(id=topping_id)
+	entries = topping.entry_set.order_by('-date_added')
+	context = {'topping':topping,'entries':entries}
+	return render(request,'pizzerias/topping.html',context)
